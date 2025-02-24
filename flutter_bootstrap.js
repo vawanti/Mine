3ee568f1bb1a747ea9591a2bedbb582605e0d392
vivekhanni <vivek279.hanni@gmail.com>
2025-02-24 23:01:01 +0530

    few fixes and correction in statements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3764839418"
+    serviceWorkerVersion: "383396069"
   }
 });
